ccaaabdb73ceecfa6fcb010db616a70bebaf176a
Gabriel <gleandro0795@gmail.com>
2021-05-03 19:31:43 -0300

    Create the project fish

---
+++ Assets/Flock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+
+    // relaciona o flocking manager para obter as variaveis
+    public FlockingManager myManager;
+
+    // Variavel de velocidade para o movimento do peixe
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/FlockingManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingManager : MonoBehaviour
+{
+
+    // Prefab para ser utilizado para instagiamento dos peixes
+    public GameObject fishPrefab;
+
+    // Numero de peixes para serem duplicados
+    public int numFish = 20;
+
+    // Lista de todos os peixes para realizar a distancia entre eles
+    public GameObject[] allFish;
+
+    //Espaçamento entre os peixes
+    public Vector3 swimLimitis = new Vector3(5, 5, 5);
+
+
+    [Header("Configuração di Cardume")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+
+    private void Start()
+    {
+        allFish = new GameObject[numFish];
+
+        // Realiza o posicionamento dos peixes da lista com uma posição aleatoria dentro do range estabelecido
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimitis.x, swimLimitis.x), Random.Range(-swimLimitis.y, swimLimitis.y), Random.Range(-swimLimitis.z, swimLimitis.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
+    }
+}
